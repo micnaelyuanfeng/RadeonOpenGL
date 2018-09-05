b2e834250bbb1b27308121c17e866d155f3c3836
michaelyuanfeng <michaelyuan.feng@mail.utoronto.ca>
2018-09-05 07:29:18 -0400

    Update buffers.cpp

--- buffers.cpp
+++ buffers.cpp
@@ -4,25 +4,38 @@
 #include "GL/glew.h" //should above glut
 #include "GL/glut.h"
 #include "SDL/SDL.h"
+#include "GLM/glm.hpp"
+#include "GLM/gtc/matrix_transform.hpp"
 
-
-const char* VERTEXT_SHADER =
+const char* VERTEXT11_SHADER =
 "#version 330 core\n"
 "layout(location = 0) in vec3 vertexPosition_modelspace;\n"
 "void main()\n"
 "{\n"
 "	gl_Position.xyz = vertexPosition_modelspace;\n"
-"	gl_position     = 1.0;\n"
+"	gl_Position.w   = 1.0;\n"
 "}\n";
 
-const char* FRAGMENT_SHADER = 
+const char* FRAGMENT_SHADER =
 "#version 330 core\n"
+"in vec3 fragmentColor;\n"
 "out vec3 color;\n"
 "void main()\n"
 "{\n"
-"color = vec3(0,0,0);\n"
+"color = fragmentColor;\n"
 "}\n";
 
+
+const char* VERTEXT_SHADER =
+"#version 330 core\n"
+"layout(location = 0) in vec3 vertexPosition_modelspace;\n"
+"layout(location = 1) in vec3 vertexColor;\n"
+"uniform mat4 MVP;\n"
+"out vec3 fragmentColor;\n"
+"void main(){\n"
+"gl_Position =  MVP * vec4(vertexPosition_modelspace,1);\n"
+"fragmentColor = vertexColor;\n"
+"}\n";
 //linear interpolation is done by gfx card
 
 using namespace std;
@@ -32,31 +45,91 @@ using namespace std;
 //In defeult OpenGL space, there is no matrixes defined
 // -1.0 to 1.0 for x, y, z
 //Normal way
-GLfloat vertices[] = { -0.5, -0.5, 0.0, //v1
-						//0.0,  1.0, 1.0, //v1c
-						0.5, -0.5, 0.0, //v2
-						//0.0,  0.0, 1.0, //v2c
-						0.0,  0.5, 0.0,
-	//0.0,  1.0, 1.0 ,
-	-0.7, 0.0, 0.0, //v1
-	//0.0,  0.0, 1.0, //v1c
-	0.5, 0.0, 0.0, //v2
-	//0.0,  0.0, 1.0, //v2c
-	0.0,  0.5, 0.0,
-	//0.0,  1.0, 1.0 };//v3
+GLfloat vertices[] = {  -1.0f,-1.0f,-1.0f, // triangle 1 : begin
+						-1.0f,-1.0f, 1.0f,
+						-1.0f, 1.0f, 1.0f, // triangle 1 : end
+						 1.0f, 1.0f,-1.0f, // triangle 2 : begin
+						-1.0f,-1.0f,-1.0f,
+						-1.0f, 1.0f,-1.0f, // triangle 2 : end
+						 1.0f,-1.0f, 1.0f,
+						-1.0f,-1.0f,-1.0f,
+						 1.0f,-1.0f,-1.0f,
+						 1.0f, 1.0f,-1.0f,
+						 1.0f,-1.0f,-1.0f,
+						-1.0f,-1.0f,-1.0f,
+						-1.0f,-1.0f,-1.0f,
+						-1.0f, 1.0f, 1.0f,
+						-1.0f, 1.0f,-1.0f,
+						 1.0f,-1.0f, 1.0f,
+						-1.0f,-1.0f, 1.0f,
+						-1.0f,-1.0f,-1.0f,
+						-1.0f, 1.0f, 1.0f,
+						-1.0f,-1.0f, 1.0f,
+						 1.0f,-1.0f, 1.0f,
+						 1.0f, 1.0f, 1.0f,
+						 1.0f,-1.0f,-1.0f,
+						 1.0f, 1.0f,-1.0f,
+						 1.0f,-1.0f,-1.0f,
+						 1.0f, 1.0f, 1.0f,
+						 1.0f,-1.0f, 1.0f,
+						 1.0f, 1.0f, 1.0f,
+						 1.0f, 1.0f,-1.0f,
+						-1.0f, 1.0f,-1.0f,
+						 1.0f, 1.0f, 1.0f,
+						-1.0f, 1.0f,-1.0f,
+						-1.0f, 1.0f, 1.0f,
+						 1.0f, 1.0f, 1.0f,
+						-1.0f, 1.0f, 1.0f,
+						 1.0f,-1.0f, 1.0f
 };
-//Color = RGB channels R      G    B
+//Color = RGB channels     R      G    B
+static const GLfloat colorBufferData[] = {
+						1.0f,  0.0f,  0.0f,
+						1.0f,  0.0f,  0.0f,
+						1.0f,  0.0f,  0.0f,
+						0.822f,  0.569f,  0.201f,
+						0.435f,  0.602f,  0.223f,
+						0.310f,  0.747f,  0.185f,
+						0.597f,  0.770f,  0.761f,
+						0.559f,  0.436f,  0.730f,
+						0.359f,  0.583f,  0.152f,
+						0.483f,  0.596f,  0.789f,
+						0.559f,  0.861f,  0.639f,
+						0.195f,  0.548f,  0.859f,
+						0.014f,  0.184f,  0.576f,
+						0.771f,  0.328f,  0.970f,
+						0.406f,  0.615f,  0.116f,
+						0.676f,  0.977f,  0.133f,
+						0.971f,  0.572f,  0.833f,
+						0.140f,  0.616f,  0.489f,
+						0.997f,  0.513f,  0.064f,
+						0.945f,  0.719f,  0.592f,
+						0.543f,  0.021f,  0.978f,
+						0.279f,  0.317f,  0.505f,
+						0.167f,  0.620f,  0.077f,
+						0.347f,  0.857f,  0.137f,
+						0.055f,  0.953f,  0.042f,
+						0.714f,  0.505f,  0.345f,
+						0.783f,  0.290f,  0.734f,
+						0.722f,  0.645f,  0.174f,
+						0.302f,  0.455f,  0.848f,
+						0.225f,  0.587f,  0.040f,
+						0.517f,  0.713f,  0.338f,
+						0.053f,  0.959f,  0.120f,
+						0.393f,  0.621f,  0.362f,
+						0.673f,  0.211f,  0.457f,
+						0.820f,  0.883f,  0.371f,
+						0.982f,  0.099f,  0.879f
+};
 
-GLuint VBOID;
+GLuint VBOID;       //VBO1
+GLuint colorBuffer; //VBO2
 
 void display()
 {
 	//For each frame = reset background color defined by glClearColor | depth value
 	//reset for redraw                need to add this if using depth information
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	//load shader
-	
 
 	//Start Drawing
 	//Tell openGl read vertices data from an array
@@ -66,6 +139,7 @@ void display()
 	//new solution to draw using data in gfx buffer
 	glBindBuffer(GL_ARRAY_BUFFER, VBOID); //activate buffer
 
+
 	//Tell openGL structure of array = how to parse the data
 	// 3 = 3D; 2 = 2D 
 	// e.g each 3 value in this array represents a vertices
@@ -74,7 +148,11 @@ void display()
 	//Draw shape type
 	//Linear interpolation and restariztion
 
-	glDrawArrays(GL_TRIANGLES, 0, 6); //send commands to gfx card
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+	glDrawArrays(GL_TRIANGLES, 0, 12 * 3); //send commands to gfx card
 	//glDrawArrays(GL_TRIANGLES, 1, 3);
 	//glDrawArrays(GL_TRIANGLES, 2, 3);
 
@@ -86,13 +164,13 @@ void display()
 
 void reshape(int w, int h)
 {
-	
+
 }
 
 void initOpenGL()
 {
 	//set background color only
-	glClearColor(0.0, 1.0, 0.0, 0.0);
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
 	//Tell ogl use depth information
 	//ogl will do depth test
@@ -115,6 +193,14 @@ void initOpenGL()
 	glGenBuffers(1, &VBOID);
 	glBindBuffer(GL_ARRAY_BUFFER, VBOID);                     //not change very often
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glGenBuffers(1, &colorBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colorBufferData), colorBufferData, GL_STATIC_DRAW);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	//unbined buffer
 	//glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -126,29 +212,42 @@ void initOpenGL()
 	int InfoLogLength = 0;
 
 	glShaderSource(VertexShaderID, 1, &VERTEXT_SHADER, NULL);
-	assert(result != GL_TRUE);
 	glCompileShader(VertexShaderID);
 
 	glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &result);
+	assert(result == GL_TRUE);
 	glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
 
 	result = GL_FALSE;
 	glShaderSource(FragmentShaderId, 1, &FRAGMENT_SHADER, NULL);
-	assert(result != GL_TRUE);
 	glCompileShader(FragmentShaderId);
 
 	glGetShaderiv(FragmentShaderId, GL_COMPILE_STATUS, &result);
+	assert(result == GL_TRUE);
 	glGetShaderiv(FragmentShaderId, GL_INFO_LOG_LENGTH, &InfoLogLength);
 
-	//Link
 	GLuint programID = glCreateProgram();
 
 	glAttachShader(programID, VertexShaderID);
 	glAttachShader(programID, FragmentShaderId);
 	glLinkProgram(programID);
 
-	glUseProgram(programID);
+	GLuint matrixID = glGetUniformLocation(programID, "MVP");
 
+	glm::mat4 projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.01f, 100.0f);
+
+	glm::mat4 view = glm::lookAt(
+		glm::vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
+		glm::vec3(0, 0, 0), // and looks at the origin
+		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+	);
+
+	glm::mat4 model = glm::mat4(1.0f);
+
+	glm::mat4 MVP = projection * view * model;
+
+	glUseProgram(programID);
+	glUniformMatrix4fv(matrixID, 1, GL_FALSE, &MVP[0][0]);
 }
 
 void idle()
@@ -168,9 +267,9 @@ int main(int argc, char **argv)
 
 	bool running = true;
 
-	mainWindow = SDL_CreateWindow("Gfx Render", SDL_WINDOWPOS_CENTERED, 
-		                           SDL_WINDOWPOS_CENTERED, 640, 480, 
-		                           SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+	mainWindow = SDL_CreateWindow("Gfx Render", SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED, 640, 480,
+		SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 
 	if (mainWindow == NULL)
 	{
@@ -201,7 +300,7 @@ int main(int argc, char **argv)
 
 			}
 		}
-		
+
 		display();
 
 		SDL_GL_SwapWindow(mainWindow);
